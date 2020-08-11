6434ef34b488db6e8ef699b95d0bcc2eb0fb43e1
xiwang.guo <xiwang.guo@zkj.com>
2020-08-11 15:18:44 +0800

    master init

--- app/src/main/java/com/example/dagger2demo/MainActivity.java
+++ app/src/main/java/com/example/dagger2demo/MainActivity.java
@@ -1,27 +1,17 @@
 package com.example.dagger2demo;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.dagger2demo.test.Student;
-
-import javax.inject.Inject;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
-    @Inject Student student;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if(student == null){
-            Log.d(TAG, "onCreate: null");
-        }
-
     }
 }
